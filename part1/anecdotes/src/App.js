ef6926e16f79fb5b94520114b4ae2ba27187cb18
Dominic London <dominic.j.london@outlook.com>
2022-12-10 20:00:34 +0000

    1.13 step2

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -5,8 +5,8 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-const Button = (props) => {
-  return <button onClick={props.onClick}>next anecdote</button>
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>
 }
 
 const App = () => {
@@ -19,14 +19,26 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
-   
+  const scores = Array(anecdotes.length).fill(0)
+
   const [selected, setSelected] = useState(0)
+  const [votes, setVote] = useState(scores)
+
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVote(copy)
+  }
+
+  const handleNext = () => setSelected(getRandomInt(anecdotes.length - 1))
 
   return (
     <div>
       {anecdotes[selected]}
       <br></br>
-      <Button onClick={() => setSelected(getRandomInt(anecdotes.length-1))} />
+      <p>has {votes[selected]} votes</p>
+      <Button onClick={handleNext} text="next anecdote" />
+      <Button onClick={handleVote} text="vote" />
     </div>
   )
 }
