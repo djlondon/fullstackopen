972ce375daef5a2400fdb5c0a62f2a8a83a171e6
Dominic London <dominic.j.london@outlook.com>
2022-12-14 16:36:36 +0000

    2.5 modules

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,28 +1,5 @@
 import React from 'react'
-
-const Header = ({ name }) => (
-  <h2>{name}</h2>
-)
-
-const Part = ({ part }) => (
-  <p>{part.name} {part.exercises}</p>
-)
-
-const Total = ({ total }) => (
-  <p><strong>total of {total} exercises</strong></p>
-)
-
-const Course = ({ course }) => {
-  const totalExercies = course.parts
-    .map(v => v.exercises)
-    .reduce((c, v) => (c + v), 0)
-  return (<div>
-    <Header name={course.name} />
-    {course.parts.map(v => <Part key={v.name} part={v} />)}
-    <Total total={totalExercies} />
-  </div>)
-}
-
+import {Course} from './components/course'
 
 const App = () => {
   const courses = [
