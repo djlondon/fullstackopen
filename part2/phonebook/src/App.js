342b180ad469c33cc27ec0fc91c15f94dc23a23b
Dominic London <dominic.j.london@outlook.com>
2022-12-14 17:58:01 +0000

    2.{6,7} step{2,3}

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    // debugger
+    if (persons.map(person => person.name).includes(newName)) {
+      alert(`${newName} is already in the phonebook`)
+    } else {
+      setPersons(persons.concat({name : newName}))
+    }
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input
+            value={newName}
+            onChange={handleNameChange}
+            />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <li key={person.name}>{person.name}</li>)}
+    </div>
+  )
+}
+
+export default App
