d9d19bc0ef8b578d05fdedc9dfe953409ead7047
Dominic London <dominic.j.london@outlook.com>
2023-01-17 14:59:24 +0000

    3.9 step9

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 const getData = (request) => (
-    request.then(response => response.data)
+  request.then(response => response.data)
 )
 
 const getAll = () => getData(axios.get(baseUrl))
